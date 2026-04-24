725cf5eb0c874519fc8cd408890bddbeda7f4ce4
PiX <69745008+pixincreate@users.noreply.github.com>
2026-04-24 20:21:09 +0530

    Add esbuild build script

---
+++ build.mjs
@@ -0,0 +1,12 @@
+import * as esbuild from "esbuild"
+
+await esbuild.build({
+  entryPoints: ["./src/plugin/index.ts"],
+  bundle: true,
+  outfile: "./dist/index.js",
+  format: "esm",
+  platform: "node",
+  sourcemap: true,
+})
+
+console.log("Built!")
